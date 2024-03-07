793a6025498a863d0295cf931c0e7cc969f95120
Oskang09 <developer.oskang@gmail.com>
2024-03-08 00:54:25 +0800

    fix: og seo

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,8 @@
 export const AppConfig = {
   site_name: 'Oska Ng',
   title: 'Oska Ng',
-  description: 'About personal projects, experiences and events',
+  description:
+    'More about Oska Ng through details about personal projects, experiences, and events.',
   author: 'Oska Ng',
   locale_region: 'en-us',
   locale: 'en',
