e9cc600ff18763d82171987fca489a637f4a58dc
Oskang09 <developer.oskang@gmail.com>
2024-06-22 04:59:32 +0800

    fix: screen issues

--- src/games/index.tsx
+++ src/games/index.tsx
@@ -1,5 +1,5 @@
 import type { WebGLEngine } from '@galacean/engine';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { init as playfulpsyduck } from './playfulpsyduck';
 
@@ -14,6 +14,14 @@ const games: {
 };
 
 const Game = (props: IGameProps) => {
+  const [canvasStyle, setCanvasStyle] = useState<React.CSSProperties>({
+    paddingLeft: 0,
+    paddingRight: 0,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    display: 'block',
+  });
+
   useEffect(() => {
     // eslint-disable-next-line func-names
     (async function () {
@@ -24,18 +32,56 @@ const Game = (props: IGameProps) => {
 
   return (
     <div>
-      <canvas
-        id="game"
-        width="295"
-        height="650"
+      <canvas id="game" width="295" height="650" style={canvasStyle} />
+      <button
+        type="button"
+        onClick={() => {
+          const fullScreenStyles: React.CSSProperties = {
+            position: 'fixed',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+          };
+          switch (true) {
+            case window.innerWidth >= 1900:
+              fullScreenStyles.width = '30%';
+              break;
+            case window.innerWidth >= 1500:
+              fullScreenStyles.width = '40%';
+              break;
+            case window.innerWidth >= 1280:
+              fullScreenStyles.width = '50%';
+              break;
+            case window.innerWidth >= 1000:
+              fullScreenStyles.width = '60%';
+              break;
+            case window.innerWidth >= 800:
+              fullScreenStyles.width = '70%';
+              break;
+            case window.innerWidth >= 550:
+              fullScreenStyles.width = '80%';
+              break;
+            default:
+          }
+          setCanvasStyle({
+            ...canvasStyle,
+            ...fullScreenStyles,
+          });
+        }}
+        className="mb-2 me-2 rounded-lg bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 px-5 py-2.5 text-center text-sm font-medium text-white shadow-lg shadow-purple-500/50 hover:bg-gradient-to-br focus:outline-none focus:ring-4 focus:ring-purple-300 dark:shadow-lg dark:shadow-purple-800/80 dark:focus:ring-purple-800"
         style={{
           paddingLeft: 0,
           paddingRight: 0,
+          marginTop: 30,
           marginLeft: 'auto',
           marginRight: 'auto',
           display: 'block',
+          width: 200,
         }}
-      />
+      >
+        FullScreen ( Mobile )
+      </button>
     </div>
   );
 };
