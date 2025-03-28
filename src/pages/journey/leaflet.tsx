49adbf14889a51ebd59a611d461413325df0a14a
Oska Ng <oska.ng@feedme.cc>
2025-03-28 20:08:39 +0800

    feat: add sample

--- src/pages/journey/leaflet.tsx
+++ src/pages/journey/leaflet.tsx
@@ -6,52 +6,60 @@ import 'swiper/css/effect-cards';
 import L from 'leaflet';
 import { useEffect } from 'react';
 
-const matches = import.meta.glob('../../journey/**.json', { eager: true });
-const journeys = Object.values(matches);
-
-const generateSwiperHTML = (images: string[]) => {
-  return `
-    <swiper-container effect="cards" grab-cursor="true">
-      ${images.map(
-        (x) => `
-        <swiper-slide>
-          <img
-            style="width: 100%; height: 100%; object-fit: cover;"
-            src="${x}" 
-          />
-        </swiper-slide>
-        `
-      )}
-    </swiper-container>
-    <div class="arrow"></div>
-  `;
+type Journey = {
+  date: string;
+  lat: number;
+  lng: number;
+  images: string[];
 };
 
-// image path: assets/journey/{image_path}
+const matches: Record<string, { default: Journey }> = import.meta.glob(
+  '../../journey/**.json',
+  { eager: true }
+);
+const journeys = Object.values(matches).flatMap((x) => x.default);
+
+const generateSwiperHTML = (date: string, images: string[]) => {
+  return `
+    <div class="swiper-wrapper">
+      <div class="swiper-title">${date}</div>
+      <swiper-container effect="cards" grab-cursor="true">
+        ${images.map(
+          (x) => `
+          <swiper-slide>
+            <img
+              style="width: 100%; height: 100%; object-fit: cover;"
+              src="${x}" 
+            />
+          </swiper-slide>
+          `
+        )}
+      </swiper-container>
+      <div class="arrow"></div>
+    </div>
+  `;
+};
 
 const Leaflet = () => {
   useEffect(() => {
     const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     const map = L.map('leaflet', { attributionControl: false }).setView(
       [4.2105, 101.9758],
-      10
+      7
     );
     L.tileLayer(tileUrl).addTo(map);
 
-    // loop journeys and render
-    console.log(journeys);
-
-    const customIcon = L.divIcon({
-      html: generateSwiperHTML(['']),
-      iconSize: [32, 32],
-      iconAnchor: [16, 32],
+    journeys.forEach((journey) => {
+      const customIcon = L.divIcon({
+        html: generateSwiperHTML(
+          journey.date,
+          journey.images.map((x: string) => `/assets/journey/${x}`)
+        ),
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+      });
+      L.marker([journey.lat, journey.lng], { icon: customIcon }).addTo(map);
     });
-
-    L.marker([5.2105, 101.9758], { icon: customIcon }).addTo(map);
-    L.marker([4.2105, 101.9758], { icon: customIcon }).addTo(map);
-    L.marker([3.2105, 101.9758], { icon: customIcon }).addTo(map);
-    L.marker([4.2105, 100.9758], { icon: customIcon }).addTo(map);
-    L.marker([4.2105, 102.9758], { icon: customIcon }).addTo(map);
   }, []);
 
   return <div id="leaflet" style={{ height: 600, width: '100%' }} />;
